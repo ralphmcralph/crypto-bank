4cee143c26819a8038a2172b77b2cca238a8b678
ralphmcralph <rhmenm@gmail.com>
2025-04-29 21:57:36 +0200

    Initial commit

---
+++ contracts/CryptoBank/CryptoBank.sol
@@ -0,0 +1,20 @@
+// License
+// SPDX-License-Identifier: GPL-3.0
+
+// Solidity version
+pragma solidity 0.8.24;
+
+contract CryptoBank {
+
+//  Functions:
+    // 1. Deposit ether
+    // 2. Withdraw ether
+
+
+// Rules:
+    // 1. Multiuser
+    // 2. Only can deposit ether
+    // 3. Only can withdraw own ether
+
+
+}
